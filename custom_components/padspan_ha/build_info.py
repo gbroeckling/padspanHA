844ba57319b29050197647fca27efb48eab1a3e3
Garry <53913193+gbroeckling@users.noreply.github.com>
2026-08-12 14:24:48 -0700

    release v0.30.0

--- custom_components/padspan_ha/build_info.py
+++ custom_components/padspan_ha/build_info.py
@@ -4,8 +4,8 @@
 # See LICENSE file or https://www.gnu.org/licenses/gpl-3.0.html
 """Generated at build time. Used to prove what version is actually installed."""
 
-BUILD_VERSION = "0.29.2"
-BUILD_ID = "20260812T195854Z"
+BUILD_VERSION = "0.30.0"
+BUILD_ID = "20260812T212444Z"
 CHANNEL = "beta"
 
 # Backwards/for convenience
