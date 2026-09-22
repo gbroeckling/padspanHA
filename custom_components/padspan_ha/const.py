cb2b8adba17355ef46cec3c602a25eb0dfb7e0bb
Garry <53913193+gbroeckling@users.noreply.github.com>
2026-09-22 15:34:31 -0700

    release v0.38.68

--- custom_components/padspan_ha/const.py
+++ custom_components/padspan_ha/const.py
@@ -19,7 +19,7 @@ Organisation:
 
 DOMAIN = "padspan_ha"
 NAME = "PadSpan HA"
-VERSION = "0.38.67"
+VERSION = "0.38.68"
 
 # ── Config-flow option keys ───────────────────────────────────────────────────
 CONF_ENABLE_CLOUD = "enable_cloud"
